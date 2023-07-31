a91c504f2f7f5d4ba93b5593142e287b28b5edda
zedor <robert.tretler@gmail.com>
2023-07-31 02:54:21 +0200

    merged API from separate project

---
+++ ASP Seminar/Controllers/ProductApiController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_Seminar.Data;
+using ASP_Seminar.Models;
+
+namespace ASP_Seminar.Controllers
+{
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductApi
+        [Route("api/products")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductApiModel>>> GetProduct()
+        {
+          if (_context.Product == null)
+          {
+              return NotFound();
+          }
+
+
+            var products = await _context.Product.ToListAsync();
+
+            List<ProductApiModel> resultList = new List<ProductApiModel>();
+            foreach (var item in products)
+            {
+                ProductApiModel buff = new ProductApiModel();
+                buff.Id = item.Id;
+                buff.Title = item.Title;
+                buff.Description = item.Description;
+                buff.Quantity = item.Quantity;
+                buff.Price = item.Price;
+                buff.HasImage = item.HasImage;
+                var buffCat = _context.ProductCategory?.Where(x => x.ProductId == item.Id).ToList();
+                foreach (var item2 in buffCat)
+                {
+                    var buffName = _context.Category.FirstOrDefault(x => x.Id == item2.CategoryId);
+                    buff.Categories.Add(buffName.Title);
+                }
+                resultList.Add(buff);
+            }
+
+            return resultList;
+
+        }
+
+        // GET: api/ProductApi/5
+        [Route("api/product/{id}")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductApiModel>> GetProduct(int id)
+        {
+          if (_context.Product == null)
+          {
+              return NotFound();
+          }
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ProductApiModel buff = new ProductApiModel();
+            buff.Id = product.Id;
+            buff.Title = product.Title;
+            buff.Description = product.Description;
+            buff.Quantity = product.Quantity;
+            buff.Price = product.Price;
+            buff.HasImage = product.HasImage;
+            var buffCat = _context.ProductCategory?.Where(x => x.ProductId == product.Id).ToList();
+            foreach (var item2 in buffCat)
+            {
+                var buffName = _context.Category.FirstOrDefault(x => x.Id == item2.CategoryId);
+                buff.Categories.Add(buffName.Title);
+            }
+
+            return buff;
+        }
+
+    }
+}
---
+++ ASP Seminar/Models/ProductApiModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ASP_Seminar.Models
+{
+    public class ProductApiModel
+    {
+        public ProductApiModel()
+        {
+            Categories = new List<string>();
+        }
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public bool? HasImage { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
